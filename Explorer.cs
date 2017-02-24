a5bf3c3035a820cb6ebf7c882c1025e5730f10f7
bestserg <sergeyglad@mail.ru>
2017-02-24 15:44:59 +0300

    initial commit. console explorer

---
+++ Explorer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleExplorer
+{
+    public class Explorer
+    {
+        private int _position;
+        private DirectoryInfo _currentDir;
+        private List<FileInfo> _items;
+
+        public Explorer(string path)
+        {
+            var startDir = new DirectoryInfo(path);
+            OpenDirectory(startDir);
+            Print();
+        }
+
+        public void StepInto()
+        {
+            if (_position == 0)
+            {
+                StepBack();
+                return;
+            }
+            var item = _items[_position - 1];
+            if ((item.Attributes == FileAttributes.Directory))
+            {
+                var dir = new DirectoryInfo(item.FullName);
+                OpenDirectory(dir);
+            }
+            Print();
+        }
+
+        public void StepBack()
+        {
+            var dir = _currentDir.Parent;
+            if (dir != null)
+            {
+                OpenDirectory(dir);
+                Print();
+            }
+        }
+
+        public void NavigateDown()
+        {
+            if (_position < _items.Count)
+            {
+                _position++;
+                Print();
+            }
+        }
+
+        public void NavigateUp()
+        {
+            if (_position > 0)
+            {
+                _position--;
+                Print();
+            }
+        }
+
+        private List<FileInfo> GetDirectoryItems(string path)
+        {
+            var files = Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly);
+            var dirs = Directory.GetDirectories(path, "*.*", SearchOption.TopDirectoryOnly);
+            var list = dirs.Select(item2 => new FileInfo(item2)).ToList();
+            list.AddRange(files.Select(item => new FileInfo(item)));
+
+            return list;
+        }
+        private void OpenDirectory(DirectoryInfo dir)
+        {
+            _currentDir = dir;
+            _items = GetDirectoryItems(dir.FullName);
+            _position = 0;
+        }
+
+        private void Print()
+        {
+            Console.Clear();
+            Console.WriteLine("Current directory: {0}", _currentDir.Name);
+            Console.WriteLine("{0} {1}", _position == 0 ? ">" : " ", "..");
+            for (int i = 0; i < _items.Count; i++)
+            {
+                var item = _items[i];
+                Console.WriteLine("{0} {1}", _position == i + 1 ? ">" : " ", item.Name);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleExplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 50;
+            var startPath = "C:\\";
+            var explorer = new Explorer(startPath);
+
+            while (true)
+            {
+                var exit = false;
+                var c = Console.ReadKey();
+                switch (c.Key)
+                {
+                    case ConsoleKey.Enter:
+                        explorer.StepInto();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        explorer.NavigateUp();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        explorer.NavigateDown();
+                        break;
+                    case ConsoleKey.Backspace:
+                        explorer.StepBack();
+                        break;
+                    case ConsoleKey.Escape:
+                        exit = true;
+                        break;
+                }
+                if (exit)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
